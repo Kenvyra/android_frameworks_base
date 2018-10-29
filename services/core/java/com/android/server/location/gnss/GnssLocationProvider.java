eebc34739f5745feeedd9aa743ca40f1ee2a0a33
MSe1969 <mse1969@posteo.de>
2018-10-29 12:14:17 +0100

    SUPL: Don't send IMSI / Phone number to SUPL server
    Change-Id: I5ccc4d61e52ac11ef33f44618d0e610089885b87

--- services/core/java/com/android/server/location/gnss/GnssLocationProvider.java
+++ services/core/java/com/android/server/location/gnss/GnssLocationProvider.java
@@ -1741,6 +1741,11 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
             return;
         }
 
+        /*
+         * We don't want to tell Google our IMSI or phone number to spy on us!
+         * As devices w/o SIM card also have working GPS, providing this data does
+         * not seem to add a lot of value, at least not for the device holder
+         *
         int subId = SubscriptionManager.getDefaultDataSubscriptionId();
         if (isEmergency && mNetworkConnectivityHandler.getActiveSubId() >= 0) {
             subId = mNetworkConnectivityHandler.getActiveSubId();
@@ -1760,7 +1765,7 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
                 // This means the framework has the SIM card.
                 type = AGPS_SETID_TYPE_MSISDN;
             }
-        }
+        } */
 
         mGnssNative.setAgpsSetId(type, (setId == null) ? "" : setId);
     }
