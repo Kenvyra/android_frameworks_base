ca2a0a9eb2969eb8b1e820c47381ecad362ebb3c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-22 22:28:16 +0300

    gmscompat: forcibly disable Homegraph listener service
    It causes chain crashes on Pixel Tablet due to the missing Dock Manager app.

--- core/java/com/android/internal/gmscompat/GmsHooks.java
+++ core/java/com/android/internal/gmscompat/GmsHooks.java
@@ -116,6 +116,8 @@ public final class GmsHooks {
         }
 
         Thread.setUncaughtExceptionPreHandler(new UncaughtExceptionPreHandler());
+
+        GmcPackageManager.init(ctx);
     }
 
     static Object configUpdateLock;
